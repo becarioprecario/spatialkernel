f45d3b916a22278a3f80f1232667400704395b72
Virgilio Gomez-Rubio <Virgilio.Gomez@uclm.es>
2017-08-07 20:54:43 +0200

    Minor changes.

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ~/.R
 
-echo "CC=clang -std=c99"      >> ~/.R/Makevars
-echo "CXX=clang++"              >> ~/.R/Makevars
-echo "CXX1X=clang++ -std=c++0x" >> ~/.R/Makevars
+echo "CC=gcc -std=c99"      >> ~/.R/Makevars
+echo "CXX=g++"              >> ~/.R/Makevars
+echo "CXX1X=g++ -std=c++0x" >> ~/.R/Makevars
 
 #Modified from https://stackoverflow.com/questions/39728709/using-an-alternate-compiler-for-travis-ci-r-project-builds
