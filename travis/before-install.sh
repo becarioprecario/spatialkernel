d297092519bc485036012df4dc31e0ae0a863e09
Virgilio Gomez-Rubio <Virgilio.Gomez@uclm.es>
2017-08-08 14:18:37 +0200

    Changed travis settings to use default compiler settings.

--- travis/before-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p ~/.R
-
-echo "CC=gcc -std=c99"      >> ~/.R/Makevars
-echo "CXX=g++"              >> ~/.R/Makevars
-echo "CXX1X=g++ -std=c++0x" >> ~/.R/Makevars
-
-#Modified from https://stackoverflow.com/questions/39728709/using-an-alternate-compiler-for-travis-ci-r-project-builds
